161ae574053a52c61ff3873ae397adb0698e4531
tonnySocorro <tonnysocorro@gmail.com>
2023-05-04 20:15:48 +0200

    vets-UO296392-flutter-06-borrando un usuario

--- vets_uo296392_flutter_app/lib/pages/home_page.dart
+++ vets_uo296392_flutter_app/lib/pages/home_page.dart
@@ -54,7 +54,9 @@ class StateHomePage extends State<HomePage> {
                           }
                       });
             },
-            onLongPress: () {},
+            onLongPress: () {
+              deleteUser(context, users[index]);
+            },
             title: Text("${users[index].name} ${users[index].surname}"),
             subtitle: Text("${'Teléfono:'}${users[index].phone}"),
             leading: CircleAvatar(
@@ -93,5 +95,34 @@ class StateHomePage extends State<HomePage> {
         child: const Icon(Icons.add),
       ),
     );
+  }
+
+  deleteUser(BuildContext context, User user) {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: const Text("Borrar usuario"),
+              content: Text("Está seguro de borrar el usuario: ${user.name}."),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      users.remove(user);
+                      Navigator.pop(context);
+                    });
+                  },
+                  child: const Text(
+                    "Borrar",
+                    style: TextStyle(color: Colors.red),
+                  ),
+                ),
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text("cancelar",
+                        style: TextStyle(color: Colors.green))),
+              ],
+            ));
   }
 }
